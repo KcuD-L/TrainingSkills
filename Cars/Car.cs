efdeedc79ec5bd4c5834c0de99fcbd6467db9e67
KcuD-L <a77787877a@gmail.com>
2025-12-21 22:51:36 +0300

    Cars
    Signed-off-by: KcuD-L <a77787877a@gmail.com>

---
+++ Cars/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.Cars
+{
+    internal class Car
+    {
+        Engine engine;
+        public Car(Engine engine)
+        {
+            this.engine = engine;
+        }
+    }
+}
---
+++ Cars/Transports.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Learning.Cars
+{
+    internal class Bus : Vehicle
+    {
+        public override void Move() => Console.WriteLine("Автобус едет"); 
+    }
+
+    internal class Electrocar : Vehicle
+    {
+        public override void Move() => Console.WriteLine("Едет бесшумно"); 
+    }
+
+    internal class Horse : IMovable
+    {
+        public void Move() => Console.WriteLine("Скачет голопом");
+    }
+}
