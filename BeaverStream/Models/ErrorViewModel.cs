692b6b956d0a543bd3f2a8c73efe58952e73f5f9
KcuD-L <a77787877a@gmail.com>
2026-01-17 22:59:37 +0300

    create web

---
+++ BeaverStream/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeaverStream.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
